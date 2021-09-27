c7d36c6a140029b241ee6ee74871e287190b9e88
brandold <brandold@amazon.com>
2021-09-27 15:14:38 -0700

    bug fix in aws cli commands

--- init_workshop.sh
+++ init_workshop.sh
@@ -10,9 +10,9 @@ cd stg309-workshop
 export AWS_DEFAULT_REGION='us-west-2'
 export C9_EC2_ID=`aws ec2 describe-instances --region us-west-2 --filters Name=tag-key,Values='aws:cloud9:environment' Name=instance-state-name,Values='running' --query "Reservations[*].Instances[*].InstanceId" --output text`
 # Get file system id
-export FILESYSTEM=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[0].OutputValue' --output text)
+export FILESYSTEM=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[1].OutputValue' --output text)
 # Get access point
-export ACCESS_POINT=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[1].OutputValue' --output text)
+export ACCESS_POINT=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[0].OutputValue' --output text)
 # Mount access point
 sudo mount -t efs -o tls,accesspoint=$ACCESS_POINT $FILESYSTEM:/ /mnt/efs
 #export BUCKET_NAME=`aws s3api list-buckets --query "reverse(sort_by(Buckets, &CreationDate))[:1] | [0].Name" | tr -d '"'`
