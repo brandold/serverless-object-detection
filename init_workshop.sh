b5925e4d43e992790923cd996e96a420332736c8
brandold <brandold@amazon.com>
2021-11-21 14:32:29 -0800

    ee support

--- init_workshop.sh
+++ init_workshop.sh
@@ -9,17 +9,17 @@ cd stg309-workshop
 export AWS_DEFAULT_REGION='us-west-2'
 export C9_EC2_ID=`aws ec2 describe-instances --region us-west-2 --filters Name=tag-key,Values='aws:cloud9:environment' Name=instance-state-name,Values='running' --query "Reservations[*].Instances[*].InstanceId" --output text`
 # Get file system id
-export FILESYSTEM=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[1].OutputValue' --output text)
+export FILESYSTEM=$(aws cloudformation list-exports --query "Exports[?Name==\`FilesystemID\`].Value" --no-paginate --output text)
 # Get access point
-export ACCESS_POINT=$(aws cloudformation describe-stacks --query 'Stacks[?starts_with(StackName, `stg`)].Outputs[0].OutputValue' --output text)
-# Mount access point
-sudo mount -t efs -o tls,accesspoint=$ACCESS_POINT $FILESYSTEM:/ /mnt/efs
-# Create models folder
-sudo mkdir /mnt/efs/models
-#export BUCKET_NAME=`aws s3api list-buckets --query "reverse(sort_by(Buckets, &CreationDate))[:1] | [0].Name" | tr -d '"'`
-export WEIGHTS='https://s3.amazonaws.com/ee-assets-prod-us-east-1/modules/e685171bdc0b4b9491cc33417008744e/v1/yolo-big.weights'
+export ACCESS_POINT=$(aws cloudformation list-exports --query "Exports[?Name==\`AccessPointID\`].Value" --no-paginate --output text)
+export WEIGHTS='https://s3.amazonaws.com/ee-assets-prod-us-east-1/modules/38c8887218564adebacdda266134918f/v1/yolo-big.weights'
 pip install --upgrade pip
 aws ec2 associate-iam-instance-profile --iam-instance-profile Name=STG309WorkshopInstanceProfile --region us-west-2 --instance-id $C9_EC2_ID
 pip --disable-pip-version-check install -q boto3
 pip --disable-pip-version-check install -q chalice
 pip --disable-pip-version-check install -q opencv-python
+
+# Mount access point
+#sudo mount -t efs -o tls,accesspoint=$ACCESS_POINT $FILESYSTEM:/ /mnt/efs
+# Create models folder
+#sudo mkdir /mnt/efs/models
